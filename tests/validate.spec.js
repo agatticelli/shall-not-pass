cb84ea4ae2f779292aa9f884a10585fc5e907b37
Alan Gatticelli <alan.gatticelli@mercadolibre.com>
2019-04-17 15:22:14 -0300

    Migrate tests tu gandalf

--- tests/validate.spec.js
+++ tests/validate.spec.js
@@ -1,52 +1,78 @@
 import { expect } from 'chai';
 
-import validate from '../src';
+import Gandalf from '../src';
 
 describe('Test validate function', () => {
   it('should validate required|email with success', () => {
     const data = { email: 'usertest@example.org' };
-    const errors = validate(data, {
-      'email': 'required|email',
+    const gandalf = new Gandalf(data, {
+      email: 'required|email',
     });
 
-    expect(Object.keys(errors)).to.be.empty;
+    expect(gandalf.errors).to.be.empty;
   });
 
   it('should validate required|email with fail', () => {
     const data = { email: 'stringquenoesunmail' };
-    const errors = validate(data, {
-      'email': 'required|email',
+    const gandalf = new Gandalf(data, {
+      email: 'required|email',
     });
 
-    expect(Object.keys(errors)).to.not.be.empty;
-    expect(errors).to.haveOwnProperty('email');
-    expect(errors['email']).to.haveOwnProperty('email');
-    expect(errors['email']).to.not.haveOwnProperty('required');
+    expect(gandalf.errors).to.not.be.empty;
+    expect(gandalf.errors).to.haveOwnProperty('email');
+    expect(gandalf.errors['email']).to.haveOwnProperty('email');
+    expect(gandalf.errors['email']).to.not.haveOwnProperty('required');
   });
 
   it('should validate required|email with double fail', () => {
     const data = { email: '' };
-    const errors = validate(data, {
-      'email': 'required|email',
+    const gandalf = new Gandalf(data, {
+      email: 'email',
     });
 
-    expect(Object.keys(errors)).to.not.be.empty;
-    expect(errors).to.haveOwnProperty('email');
-    expect(errors['email']).to.haveOwnProperty('email');
-    expect(errors['email']).to.haveOwnProperty('required');
+    expect(gandalf.errors).to.not.be.empty;
+    expect(gandalf.errors).to.haveOwnProperty('email');
+    expect(gandalf.errors['email']).to.haveOwnProperty('email');
   });
 
   it('should validate between', () => {
     const data = { name: 'Dexter Morgan' };
-    let errors = validate(data, {
+    const gandalf = new Gandalf(data, {
       name: 'required|between:5,25|string',
     });
-    expect(errors).to.be.empty;
+    expect(gandalf.errors).to.be.empty;
 
-    errors = validate(data, {
-      name: 'required|between:5,10|string',
+    gandalf.revalidate({
+      rules: {
+        name: 'required|between:5,10|string',
+      },
     });
-    expect(errors).to.have.keys(['name']);
-    expect(errors['name']).to.have.keys(['between']);
+    expect(gandalf.errors).to.have.keys(['name']);
+    expect(gandalf.errors['name']).to.have.keys(['between']);
+  });
+
+  it('should validate numeric', () => {
+    const gandalf = new Gandalf({ userId: 113 }, {
+      userId: 'numeric',
+    });
+    expect(gandalf.errors).to.be.empty;
+
+    gandalf.revalidate({
+      data: {}
+    });
+    expect(gandalf.errors).to.be.empty;
+
+    gandalf.revalidate({
+      data: { userId: null },
+    });
+    expect(gandalf.errors).to.not.be.empty;
+    expect(gandalf.errors).to.haveOwnProperty('userId');
+
+    gandalf.revalidate({
+      rules: {
+        userId: 'nullable|numeric',
+      }
+    });
+    expect(gandalf.errors).to.be.empty;
   });
 });
