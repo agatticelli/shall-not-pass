1855231b88515782acc184702e2906e5a166b570
Alan Gatticelli <alan.gatticelli@mercadolibre.com>
2019-04-22 02:03:34 -0300

    Lists of params go with ... at the begining

--- resources/custom/en.js
+++ resources/custom/en.js
@@ -1,4 +1,4 @@
 module.exports = {
-  in_list: 'The field :attribute must be in the array :list',
+  in_list: 'The field :attribute must be in the array :...list',
   is_five: 'The field :attribute must be five',
 }
